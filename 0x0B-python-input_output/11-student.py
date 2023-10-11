8d91f34c0660da211256b36c142097c95b9bf9db
OheneAkuffo <Ohenekakuffo@gmail.com>
2023-10-10 19:37:44 -0700

    Write a class Student that defines a student by: (based on 10-student.py)

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    A class Student that defines a student
+"""
+
+
+class Student:
+    """Representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student instance variables"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary representation of a Student instance"""
+        if type(attrs) is list:
+            return {items: getattr(self, items) for items in attrs
+                    if hasattr(self, items)}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key in json:
+            try:
+                setattr(self, key, json[key])
+            except FileNotFoundError:
+                pass
